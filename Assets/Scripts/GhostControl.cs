bc969aa129671157b37adcad7b7bd378a129c85d
Skohell <guillaume.gravallon@gmail.com>
2017-02-18 19:01:43 +0100

    commit initial

---
+++ Assets/Scripts/GhostControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostControl : MonoBehaviour {
+
+    //vitesse du fantome
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GameObject scooby = GameObject.Find("sprite_scooby");
+        Vector2 scooby_pos = scooby.transform.position;
+
+        Vector2 position = transform.position;
+
+        if(scooby_pos.x > position.x && scooby_pos.y > position.y)
+        {
+            position = new Vector2(position.x + speed * Time.deltaTime, position.y + speed * Time.deltaTime);
+
+        }
+        else if(scooby_pos.x > position.x && scooby_pos.y < position.y)
+        {
+            position = new Vector2(position.x + speed * Time.deltaTime, position.y - speed * Time.deltaTime);
+
+        }
+        else if(scooby_pos.x < position.x && scooby_pos.y > position.y)
+        {
+            position = new Vector2(position.x - speed * Time.deltaTime, position.y + speed * Time.deltaTime);
+
+        }
+        else
+        {
+            position = new Vector2(position.x - speed * Time.deltaTime, position.y - speed * Time.deltaTime);
+
+        }
+        transform.position = position;
+	}
+}
---
+++ Assets/Scripts/Scooby_life.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scooby_life : MonoBehaviour {
+
+    int pv;
+    GameObject vie1;
+    GameObject vie2;
+    
+    SpriteRenderer vie3;
+	// Use this for initialization
+	void Start () {
+        pv = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vie3 = GameObject.Find("vie3").GetComponent<SpriteRenderer>();
+
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        vie3.sprite = Resources.Load("coeur_vide", typeof(Sprite)) as Sprite;
+    }
+}
