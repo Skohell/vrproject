19b18d0d1ea35b5b8cbcdd9b7d366afbfd0ca87b
Tanguy De Taxis Du Poët <wgokraken@gmail.com>
2017-03-17 15:13:43 +0100

    Cookie Collision Ghost

--- Assets/Scripts/CookieSpawner.cs
+++ Assets/Scripts/CookieSpawner.cs
@@ -36,6 +36,15 @@ public class CookieSpawner : MonoBehaviour {
 
         Vector2 pos = new Vector2(Random.Range(-8,8), Random.Range(-4,4));
         GameObject clone = Instantiate(cookie, pos, Quaternion.identity);
-        clone.GetComponent<CircleCollider2D>().isTrigger = false;
+        CircleCollider2D clone_component = clone.GetComponent<CircleCollider2D>();
+        clone_component.isTrigger = false;
+
+        GameObject[] ghosts = GameObject.FindGameObjectsWithTag("ghost");
+        foreach (GameObject g in ghosts)
+        {
+            Collider2D g2 = g.GetComponent<CircleCollider2D>();
+            Physics2D.IgnoreCollision(g2, clone_component);
+        }
+
     }
 }
