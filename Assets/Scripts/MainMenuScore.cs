195e9f15c93d877e6a2edafc3007832bc26256b8
Tanguy De Taxis Du Poët <wgokraken@gmail.com>
2017-03-20 15:18:55 +0100

    Best Score Menu

--- Assets/Scripts/MainMenuScore.cs
+++ Assets/Scripts/MainMenuScore.cs
@@ -7,19 +7,27 @@ using UnityEngine.UI;
 
 public class MainMenuScore : MonoBehaviour {
 
+    [SerializeField]Text var_lastScore;
+    [SerializeField]Text var_bestScore;
     [SerializeField]Text lastScore;
     [SerializeField]Text bestScore;
+    [SerializeField]Canvas MenuBest;
 
     // Use this for initialization
     void Start()
     {
-        lastScore.text = "Last Score : " + Scooby_life.pts;
+        MenuBest.enabled = false;
+        var_lastScore.text = Scooby_life.pts.ToString();
         if(File.Exists("scores.bin"))
         {
             using (BinaryReader reader = new BinaryReader(File.Open("scores.bin", FileMode.Open)))
             {
-                bestScore.text = "Best Score : " + reader.ReadInt16().ToString();
+                var_bestScore.text = reader.ReadInt16().ToString();
             }
+        }
+        if(var_lastScore == var_bestScore)
+        {
+            MenuBest.enabled = true;
         }
     }
 	// Update is called once per frame
